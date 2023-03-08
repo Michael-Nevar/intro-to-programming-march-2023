67e8fdc33ba2e0034fa0efc2429c62b5ffd7e80d
Michael Nevar <Michael_Nevar@progressive.com>
2023-03-08 16:01:13 -0500

    end of wed 8

---
+++ stringcalculator-wed-8/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿
+using Xunit.Sdk;
+
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (numbers == "") return 0;
+        numbers = numbers.Replace("\n", ",");
+        var sum = 0;
+        var numberList = numbers.Split(',');
+        foreach (var number in numberList)
+        {
+            sum += int.Parse(number);
+        }
+        return sum;
+    }
+}
---
+++ stringcalculator-wed-8/StringCalculatorTests.cs
@@ -0,0 +1,74 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("4",4)]
+    [InlineData("5",5)]
+    [InlineData("1242",1242)]
+    public void SingleDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("4,5",9)]
+    [InlineData("2,3",5)]
+    public void TwoDigits(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("4,5,6", 15)]
+    [InlineData("2,300,15", 317)]
+    public void MultipleNumbers(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("3,5,45,60", 114)]
+    [InlineData("12\n340\n1", 353)]
+    public void NewLineSplitters(string numbers, int expected)
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void DifferentDelimiters()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//;\n1;2");
+        
+        Assert.Equal(3, result);
+    }
+}
