67b1808ee25654d10df4592acc49a6c0b48d99b7
Michael Nevar <Michael_Nevar@progressive.com>
2023-03-10 15:52:43 -0500

    eod friday 10

--- CSharpSyntaxSolution/CSharpSyntax/Customer.cs
+++ CSharpSyntaxSolution/CSharpSyntax/Customer.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace CSharpSyntax;
 
-namespace CSharpSyntax
+public class Customer
 {
-    public class Customer
+    private decimal _availableCredit = 5000;
+    /// <summary>
+    /// Use this method to get the current credit limit
+    /// </summary>
+    /// <returns>What the customer's credit limit is right now</returns>
+    public decimal GetCurrentAvailableCredit()
     {
-        private decimal _availableCredit = 5000;
-        public decimal GetCurrentAvailableCredit()
-        {
-            return _availableCredit;
-        }
-
-        /// <summary>
-        /// Use this to increase the credit limit
-        /// </summary>
-        /// <param name="amount">the amount to add to the current credit limit</param> 
-        /// <param name="effectiveDate"></param>
-        public void IncreaseAvailableCredit(decimal amount, DateTimeOffset effectiveDate)
-        {
-            _availableCredit += amount;
-        }
+        return _availableCredit;
+    }
+    /// <summary>
+    /// Use this to increase the credit limit
+    /// </summary>
+    /// <param name="amount">The amount to ADD to the current credit limit</param>
+    /// <param name="effectiveData">When this should be applied5</param>
+    public void IncreaseAvailableCredit(decimal amount, DateTimeOffset effectiveData)
+    {
+        _availableCredit += amount;
     }
 }
--- CSharpSyntaxSolution/CSharpSyntax/DemoTests.cs
+++ CSharpSyntaxSolution/CSharpSyntax/DemoTests.cs
@@ -1,51 +1,56 @@
-using Newtonsoft.Json.Bson;
-using Xunit.Sdk;
 
-namespace CSharpSyntax
+namespace CSharpSyntax;
+
+public class DemoTests
 {
-    public class DemoTests
+    [Fact] // Attribute (Decorator) 
+    public void Test1()
     {
-        [Fact] // Attribute (Decorator)
-        public void Test1()
-        {
-            // Given 
-            // set the universe
-            int a = 10, b = 20;
-            int answer;
-            // When 
-            // the actual test - do something
-            answer = a + b; // what are we testing?
-            // Then
-            // observe the result - did it work as expected
-            Assert.Equal(30, answer);
-        }
+        // Given
+        // set the universe 
+        int a = 10, b = 20;
+        int answer;
+        // When
+        // the actual test - do something
+        answer = a + b; // what are we testing?
+        // Then
+        // observe the result - did it work as expected
+        Assert.Equal(30, answer);
 
-        [Theory]
-        [InlineData(10,20,30)]
-        [InlineData(2,2, 4)]
-        [InlineData(10,2, 12)]
-        public void CanAddAnyTwoIntegers(int a, int b, int expected)
-        {
-            int answer = a + b;
-            Assert.Equal(expected, answer);
-        }
+    }
 
-        [Fact]
-        public void BasicOOPStuff()
-        {
-            string myName = "    Jeff";
-            string trimmedName = myName.TrimStart();
+    [Theory]
+    [InlineData(10,20,30)]
+    [InlineData(2,2, 4)]
+    [InlineData(10,2, 12)]
+    public void CanAddAnyTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+       
+        Assert.Equal(expected, answer);
 
-            Assert.Equal("Jeff", trimmedName);
-            Assert.Equal("    Jeff", myName);
+       
+        
+    }
 
-            int myAge = 52;
+    [Fact]
+    public void BasicOOPStuff()
+    {
+        string myName = "    Jeff";
+        string trimmedName = myName.TrimStart();
 
-            Customer bob = new Customer();
+        Assert.Equal("Jeff", trimmedName);
+        Assert.Equal("    Jeff", myName);
 
-            Assert.Equal(5000, bob.GetCurrentAvailableCredit());
-            bob.IncreaseAvailableCredit(50, DateTimeOffset.Now);
-            Assert.Equal(5050, bob.GetCurrentAvailableCredit());
-        }
+        int myAge = 52;
+
+        Customer bob = new Customer();
+
+        // what is our PROMISE to people using this thing.
+
+        Assert.Equal(5000, bob.GetCurrentAvailableCredit());
+        bob.IncreaseAvailableCredit(50, DateTimeOffset.Now);
+        Assert.Equal(5050, bob.GetCurrentAvailableCredit());
     }
 }
+
